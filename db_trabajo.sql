0021c38e0df9e1fbf68be9b9405dc79c71f1fdb2
fracarg3 <78934273+fracarg3@users.noreply.github.com>
2023-02-16 18:08:09 +0100

    Add files via upload
    base de datos vacía siguiendo la guía que hemos hecho en el ipad
    
    
    Signed-off-by: fracarg3 <78934273+fracarg3@users.noreply.github.com>

---
+++ db_trabajo.sql
@@ -0,0 +1,31 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.24-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Volcando datos para la tabla 4enraya.estadopartida: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla 4enraya.fichapuesta: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla 4enraya.jugadores: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla 4enraya.partidas: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla 4enraya.tablero: ~0 rows (aproximadamente)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
